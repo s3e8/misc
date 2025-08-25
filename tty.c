83a2899a9ff6f587ea94ff5f9ea5744a4b2cd282
s3e8 <sphiax.s3e8@gmail.com>
2025-08-25 04:35:22 +0000

    this works

--- tty.c
+++ tty.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/ioctl.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,6 +10,8 @@
 
 /*** data ***/
 struct tty_cfg {
+    int rows;
+    int cols;
     struct termios orig_termios;
 };
 
@@ -54,14 +57,61 @@ char tty_read_key() {
     return c;
 }
 
-void tty_draw_rows() {
-    int y;
-    for(y = 0; y < 24; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+int get_cursor_position(int* rows, int* cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if(read(STDIN_FILENO, &buf[i], 1) == -1) break;
+        if(buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    // printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+
+    // printf("\r\n");
+    // char c;
+    // while (read(STDIN_FILENO, &c, 1) == 1) {
+    //     if(iscntrl(c)) {
+    //         printf("%d\r\n", c);
+    //     }
+    //     else {
+    //         printf("%d ('%c')\r\n", c, c);
+    //     }
+    // }
+    // tty_read_key();
+
+    return -0;
+}
+
+int get_window_size(int* r, int* c) {
+    struct winsize ws;
+
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return get_cursor_position(r, c);
+    } 
+    else {
+        *c = ws.ws_col;
+        *r = ws.ws_row;
+        return 0;
     }
 }
 
 /*** output ***/
+void tty_draw_rows() {
+    int y;
+    for(y = 0; y < cfg.rows; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void tty_refresh_screen() {
     write(STDOUT_FILENO, "\x1b[2J]", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
@@ -85,9 +135,15 @@ void tty_process_key_press() {
 }
 
 
+/*** init ***/
+
+void init_tty() {
+    if (get_window_size(&cfg.rows, &cfg.cols) == -1) die("get_window_size");
+}
 
 int main() {
   enable_raw_mode();
+  init_tty();
 
   while(1) {
     tty_refresh_screen();
