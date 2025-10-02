5d468632dde8a8feaf97bd098811a8b35d0577ab
s3e8 <sphiax.s3e8@gmail.com>
2025-10-02 19:45:59 +0000

    stuff

---
+++ fth_ops.c
@@ -0,0 +1,8 @@
+
+
+void fth_op_read_key();
+
+void fth_op_read_key()
+{
+    // tty_read_key();
+}
---
+++ trkr.c
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+
+typedef struct audio_buffer_t
+{
+    // char*   name;
+    // original_format;
+    // file_location;
+    int sample_rate;
+    int     length_in_frames;
+    bool    is_playing;
+    // ma_decoder buffer;
+    int range_start_frame;
+    int range_end_frame;
+    int loop_start_frame;
+    int loop_end_frame;
+} audio_buffer_t;
+
+// void audio_buffer_vtable =
+// {
+    
+// }
+
+void audio_callback()
+{
+    
+}
+
+
+int main()
+{
+
+}
